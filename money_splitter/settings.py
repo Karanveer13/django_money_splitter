5b02bccb1fcf6ea248ee3f74ca5d5b0503e81fd0
Karanveer13 <karanveershekhawatna13@gmail.com>
2022-03-30 23:30:31 +0530

    adding debug tastypie

--- money_splitter/settings.py
+++ money_splitter/settings.py
@@ -32,6 +32,8 @@ SECRET_KEY = 'django-insecure-8livpb-366)0k*6zl)z6sr#49q1k!h%4%j)6)m=cu#5%d2_pnj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+TASTYPIE_FULL_DEBUG = True
+
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'expenser-app-django-heroku.herokuapp.com']
 #ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
