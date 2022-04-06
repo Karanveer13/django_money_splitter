34ae3f69ce5211ab70c1f8b33c65d5a039c5aa0f
Karanveer13 <karanveershekhawatna13@gmail.com>
2022-04-06 14:45:57 +0530

    splitter

--- splitter/admin.py
+++ splitter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Profile, Profile_Friend, Group, Group_Friend, Expense, Splitter, Expense_Total, Settle
+from .models import User, Profile, Profile_Friend, Group, Group_Friend, Expense, Splitters, Expense_Total, Settle
 # Register your models here.
 
 #admin.site.register(User, UserAdmin)
@@ -9,6 +9,6 @@ admin.site.register(Profile_Friend)
 admin.site.register(Group)
 admin.site.register(Group_Friend)
 admin.site.register(Expense)
-admin.site.register(Splitter)
+admin.site.register(Splitters)
 admin.site.register(Expense_Total)
 admin.site.register(Settle)
