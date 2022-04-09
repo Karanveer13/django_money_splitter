2fd4299aee2adb3b298a75d24517e36b4c289d60
Karanveer13 <karanveershekhawatna13@gmail.com>
2022-04-09 12:29:25 +0530

    splitter

--- splitter/models.py
+++ splitter/models.py
@@ -56,6 +56,7 @@ class Expense_Splitter(models.Model):
     expense = models.ForeignKey(Expense, on_delete = models.CASCADE, related_name = "Expense_name")
     e_splitter = models.ForeignKey(Group_Friend, on_delete=models.CASCADE, related_name="Expense_Group_Friend")
     owes = models.IntegerField()
+    settle = models.BooleanField(default=False)
 
     def __str__(self):
         return  self.e_splitter.friend.p_friend.username +' in expense ' + self.expense.reason + ' owes ' + str(self.owes)
@@ -82,73 +83,3 @@ class Settle(models.Model):
 
     def __str__(self):
         return self.sender.friend.p_friend.username + ' has to pay ' + self.receiver.friend.p_friend.username + ' in group ' + self.group.name
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from pyexpat import model
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.db import models
-# from tastypie.models import create_api_key
-#
-# models.signals.post_save.connect(create_api_key, sender=User)
-#
-# TASK_PRIORITY = [
-#     ('Lazy', 'Lazy'),
-#     ('Moderate', 'Moderate'),
-#     ('Urgent', 'Urgent'),
-# ]
-#
-# class Task(models.Model):
-#     title = models.CharField(max_length=255)
-#     description = models.TextField()
-#     priority = models.CharField(max_length=12, choices=TASK_PRIORITY, default='Moderate')
-#     status = models.BooleanField(default=False)
-#     #tag = models.TextField()
-#     #link = models.TextField()
-#     creator = models.ForeignKey(User, on_delete=models.CASCADE)
-#     assigned_to = models.ManyToManyField(User, blank=True, related_name='assigned')
-#     created_at = models.DateField(auto_now_add=True)
-#     updated_at = models.DateField(auto_now=True)
-#
-#     def __str__(self):
-#         return self.title
-#
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     # image = models.ImageField(upload_to='image', blank=True)
-#     friends = models.ManyToManyField(User, blank=True, related_name="profiles")
-#     created_at = models.DateField(auto_now_add=True, null=True)
-#     updated_at = models.DateField(auto_now=True)
-#
-#     def __str__(self):
-#         return self.user.username
