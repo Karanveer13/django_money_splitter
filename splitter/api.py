e1f9465cea138732b90846fe282d141702fd822a
Karanveer13 <karanveershekhawatna13@gmail.com>
2022-04-10 21:05:35 +0530

    splitter

--- splitter/api.py
+++ splitter/api.py
@@ -13,6 +13,7 @@ from tastypie.exceptions import (
     UnsupportedSerializationFormat, UnsupportedDeserializationFormat,
 )
 from django.http import JsonResponse
+#from .serializers import Expense_Serializer
 from tastypie import http
 
 from tastypie.exceptions import BadRequest
@@ -265,11 +266,13 @@ class Expense_Resource(ModelResource):
     payer = fields.ForeignKey(Group_Friend_Resource, attribute='payer', null=True)
     #settled_by = models.ManyToManyField(Group_Friend, related_name="Expense_settled_splitters")
     settled_by = fields.ToManyField(Group_Friend_Resource, attribute='settled_by', null=True, blank=True)
-    splitters = fields.ToManyField(Group_Friend_Resource, attribute='splitters', null=True, readonly = True, full=True)
+    #splitters = fields.ToManyField(Group_Friend_Resource, attribute='splitters', null=True, readonly = True, full=True)
+    splitters = fields.ToManyField('splitter.api.Expense_Splitter_Resource', attribute=lambda bundle: bundle.obj.splitters.through.objects.filter(expense=bundle.obj ) or bundle.obj.splitters , null=True, readonly=True, full=True)
     #splitters = fields.ToManyField('splitter.api.Expense_Splitter_Resource', attribute='splitters', null=True, readonly=True, full=True)
 
     class Meta:
         queryset = Expense.objects.all()
+        #serializer_class = Expense_Serializer
         resource_name = 'expense'
         max_limit = None
         allowed_methods = ['get', 'post', 'put','delete']
@@ -298,10 +301,7 @@ class Expense_Resource(ModelResource):
 class Expense_Splitter_Resource(ModelResource):
     expense = fields.ForeignKey(Expense_Resource, attribute='expense', null=True)
     e_splitter = fields.ForeignKey(Group_Friend_Resource, attribute='e_splitter', null=True)
-    #owes = fields.IntegerField()
-    #owes = fields.IntegerField('owes', readonly=True, null=True)
-    #owes = fields.IntegerField(attribute='owes')
-    #age = fields.IntegerField(attribute='years_old', null=True)
+    #owes = fields.IntegerField(null=True)
 
     class Meta:
         queryset = Expense_Splitter.objects.all()
--- splitter/serializers.py
+++ splitter/serializers.py
@@ -1,37 +1,41 @@
-import time
-import json
-
-from django.core import serializers
-from django.core.serializers.json import DjangoJSONEncoder
-from rest_framework.serializers import ModelSerializer
-from tastypie.serializers import Serializer
-from models import Expense, Expense_Splitter
-
-class Expense_Serializer(ModelSerializer):
-    splitters = Expense_Splitter_Serializer(source='expense_splitter_set', many=True)
-
-    class Meta:
-        model = Expense
-
-class Expense_Splitter_Serializer(serializers.HyperlinkedModelSerializer):
-
-    expense = serializers.Field(source='expense')
-    e_splitter = serializers.Field(source='e_splitter')
-
-    class Meta:
-        model = Expense_Splitter
-
-        fields = ('id', 'name', 'join_date', )
-
-
-
-
-class MemberSerializer(ModelSerializer):
-    groups = MembershipSerializer(source='membership_set', many=True)
-
-    class Meta:
-        model = Member
-
-class GroupSerializer(ModelSerializer):
-    class Meta:
-        model = Group
+# import time
+# import json
+#
+# from django.core import serializers
+# from django.core.serializers.json import DjangoJSONEncoder
+# #from rest_framework.serializers import ModelSerializer
+# from tastypie.serializers import Serializer
+# from tastypie.serializers import Serializer
+# from .models import Expense, Expense_Splitter
+#
+# class Expense_Splitter_Serializer(Serializer):
+#
+#     expense = serializers.Field(source='expense.id')
+#     e_expense = serializers.Field(source='e_expense.id')
+#
+#     class Meta:
+#         model = Expense_Splitter
+#
+#         fields = ('expense', 'e_splitter', 'owes', )
+#
+#
+#
+# class Expense_Serializer(Serializer):
+#     splitters = Expense_Splitter_Serializer(source='expense_splitter_set', many=True)
+#
+#     class Meta:
+#         model = Expense
+#
+#
+#
+#
+#
+# # class MemberSerializer(ModelSerializer):
+# #     groups = MembershipSerializer(source='membership_set', many=True)
+# #
+# #     class Meta:
+# #         model = Member
+# #
+# # class GroupSerializer(ModelSerializer):
+# #     class Meta:
+# #         model = Group
